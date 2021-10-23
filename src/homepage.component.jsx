176ee74454c071d5bd177e4470ae9f888caf35d8
Ankit Sharma <sankit72@gmail.com>
2021-10-23 20:15:51 +0530

    created app and added files for homepage

---
+++ src/homepage.component.jsx
@@ -0,0 +1,14 @@
+import './homepage.styles.scss'
+
+const Homepage = () => (
+    <div className="homepage">
+        <div className='menu-item'>
+            <div className="content">
+                <h1 className="title">JACKETS</h1>
+                <h3 className="subtitle">Shop now</h3>
+            </div>
+        </div>
+    </div>
+);
+
+export default Homepage;
