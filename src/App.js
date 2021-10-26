d78f8f6be36f044f352cde78f9cf0b4387a34ec4
Ankit Sharma <sankit72@gmail.com>
2021-10-26 20:56:38 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './App.css';
 import Homepage from './homepage.component';
 
-const App = () => <div><Homepage /></div>
+const Hats = (props) => {
+    console.log(props)
+    return <h1>Testing</h1>;
+}
+
+const App = () => <div>
+    <Router>
+        <Switch>
+            <Route exact path="/" component={Homepage}></Route>
+            <Route path="/hats" component={Hats}></Route>
+        </Switch>
+    </Router>
+</div>
 
 export default App;
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,13 +1,17 @@
+import { withRouter } from 'react-router';
 import './menu-items.styles.scss'
 
-const MenuItem = (props) => <div className={`${props.size} menu-item`}>
-    <div className="background-image" style={{
-        backgroundImage: `url(${props.imageUrl})`
-    }}></div>
-    <div className="content">
-        <h1 className="title">{props.title.toUpperCase()}</h1>
-        <h3 className="subtitle">SHOP NOW</h3>
+const MenuItem = ({ size, history, title, imageUrl, match, linkUrl }) =>
+    <div
+        className={`${size} menu-item`}
+        onClick={() => history.push(`${match.url}${linkUrl}`)}>
+        <div className="background-image" style={{
+            backgroundImage: `url(${imageUrl})`
+        }}></div>
+        <div className="content">
+            <h1 className="title">{title.toUpperCase()}</h1>
+            <h3 className="subtitle">SHOP NOW</h3>
+        </div>
     </div>
-</div>
 
-export default MenuItem;
+export default withRouter(MenuItem);
