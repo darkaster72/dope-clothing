208710aae1b84fdaa3ef74b6ecb480329ba10fe9
Ankit Sharma <sankit72@gmail.com>
2021-11-09 21:38:30 +0530

    added cart dropdown

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,11 +1,17 @@
+import { connect } from 'react-redux';
 import { ReactComponent as ShoppingBag } from '../../assets/shopping-bag.svg'
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import './cart-icon.styles.scss'
 
-const CartIcon = props => (
-    <div className="cart-icon">
+const CartIcon = ({ toggleCartHidden }) => (
+    <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingBag className="shopping-icon" />
         <span className="item-count">0</span>
     </div>
 );
 
-export default CartIcon;
+const mapDispatchToProps = dispatch => ({
+    toggleCartHidden: () => dispatch(toggleCartHidden())
+})
+
+export default connect(null, mapDispatchToProps)(CartIcon);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 class Header extends React.Component {
 
@@ -24,12 +25,14 @@ class Header extends React.Component {
                     }
                     <CartIcon className="options" />
                 </div>
+                {!this.props.cartDropdownHidden && <CartDropdown />}
             </div>
         );
     }
 }
 const mapStateToProps = state => ({
     currentUser: state.user.currentUser,
+    cartDropdownHidden: state.cart.hidden
 })
 
 export default connect(mapStateToProps)(Header);
