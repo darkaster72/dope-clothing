bb7fcf6f4f31a5d21528bedc11a80420f3c5ead0
Ankit Sharma <sankit72@gmail.com>
2021-11-15 23:57:22 +0530

    added more hooks

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,29 +1,34 @@
-import { connect } from 'react-redux';
-import { CartItem } from '../cart-item/cart-item.component';
-import { CustomButton } from '../custom-button/custom-button.component'
-import { selectCartItems } from '../../redux/cart/cart.selectors'
-
-import './cart-dropdown.styles.scss'
-import { withRouter } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { CartItem } from '../cart-item/cart-item.component';
+import { CustomButton } from '../custom-button/custom-button.component';
+import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems, history, dispatch }) => (
-    <div className="cart-dropdown">
-        <div className="cart-items">
-            {cartItems.length
-                ? cartItems.map(item => <CartItem key={item.id} item={item} />)
-                : <span className="empty-message">Your cart is empty</span>
-            }
+
+const CartDropdown = () => {
+    const history = useHistory()
+    const dispatch = useDispatch()
+    const cartItems = useSelector(selectCartItems)
+
+    return (
+        <div className="cart-dropdown">
+            <div className="cart-items">
+                {cartItems.length
+                    ? cartItems.map(item => <CartItem key={item.id} item={item} />)
+                    : <span className="empty-message">Your cart is empty</span>}
+            </div>
+            <CustomButton onClick={() => {
+                history.push('/checkout');
+                dispatch(toggleCartHidden());
+            }}>GO TO CHECKOUT</CustomButton>
         </div>
-        <CustomButton onClick={() => {
-            history.push('/checkout');
-            dispatch(toggleCartHidden() )
-        }}>GO TO CHECKOUT</CustomButton>
-    </div>
-);
+    );
+};
 
 const mapStateToProps = state => ({
     cartItems: selectCartItems(state)
 })
 
-export default withRouter(connect(mapStateToProps)(CartDropdown));
+export default CartDropdown;
--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,22 +1,22 @@
-import { connect } from "react-redux";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router";
 import CollectionItem from "../../components/collection-item/collection-item.component";
 import { selectCollection } from "../../redux/shop/shop.selector";
-import './collection.styles.scss'
+import './collection.styles.scss';
 
-const CollectionPage = ({ collection: { title, items } }) => (
-    <div className="collection-page">
-        <h1 className="title">{title}</h1>
-        <div className="items">
-            {
-                items.map((item) =>
-                    <CollectionItem key={item.id} item={item} />)
-            }
+const CollectionPage = () => {
+    const { collectionId } = useParams()
+    const collection = useSelector(selectCollection(collectionId))
+    const { title, items } = collection
+
+    return (
+        <div className="collection-page">
+            <h1 className="title">{title}</h1>
+            <div className="items">
+                {items.map((item) => <CollectionItem key={item.id} item={item} />)}
+            </div>
         </div>
-    </div>
-)
+    );
+}
 
-const mapStateToProps = (state, ownProps) => ({
-    collection: selectCollection(ownProps.match.params.collectionId)(state)
-})
-
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
